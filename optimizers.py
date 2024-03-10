bd3638e1398f80d39278899a9bd3744aab46efc3
MathoVerse100 <140441695+MathoVerse100@users.noreply.github.com>
2024-03-10 18:29:48 +0300

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 
+
 def gradient_descent(function: Union[sp.Expr, sp.Symbol], parameters: List[sp.Symbol], alpha: float, iterations: int=100, guess=None):
     if not guess:
         guess = np.array([1 for i in range(len(parameters))])
